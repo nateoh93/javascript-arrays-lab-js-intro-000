4115fef68107b4dbdc75c041b7d727c8ac91f14a
Nathan Oh <nathanoh93@gmail.com>
2020-02-22 23:41:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var kittens = ['Milo', 'Otis', 'Garfield']
  //define your array here
 
 // Add your functions and code here
-function destructivelyAppendKitten(kittens, name) {
-  return kittens.concat("Ralph")
+function destructivelyAppendKitten(name) {
+  return kittens.concat(name)
 }
+destructivelyAppendKitten("Ralph")
