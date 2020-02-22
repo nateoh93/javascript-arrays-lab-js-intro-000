fb581c138b0599f103369e24101c8fdf25b2b1b2
Nathan Oh <nathanoh93@gmail.com>
2020-02-22 23:38:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ var kittens = ['Milo', 'Otis', 'Garfield']
 
 // Add your functions and code here
 function destructivelyAppendKitten(kittens, name) {
-  kittens.push(name)
+  kittens.push[name]
   return kittens
 }
